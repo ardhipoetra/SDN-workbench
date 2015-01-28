683c0c2483b8e6169b5d26d24ca824b9aaa4b1e9
ardhipoetra <ardhipoetra@yahoo.com>
2015-01-28 03:20:14 -0800

    add simple monitoring module in sch4_mon

---
+++ sch4_mon.py
@@ -0,0 +1,310 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from threading import Thread
+
+import time
+import os,sys
+import subprocess 
+import csv
+import StringIO
+import iptc
+import socket, errno
+
+
+HOSTS = 6
+
+p1_log = open('logs-example/log.p1.txt', 'w')
+p2_log = open('logs-example/log.p2.txt', 'w')
+
+flag_mon66 = 0
+flag_mon67 = 0
+
+flag_exit = False
+
+def closePort(port):
+	rule=iptc.Rule()
+	rule.protocol = "tcp"
+	match = rule.create_match("tcp")
+	match.dport = str(port)
+	chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+	rule.target = rule.create_target("DROP")
+	chain.insert_rule(rule)
+
+def unClosePort(port):
+	rule=iptc.Rule()
+	rule.protocol = "tcp"
+	match = rule.create_match("tcp")
+	match.dport = str(port)
+	chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+	rule.target = rule.create_target("DROP")
+	chain.delete_rule(rule)
+
+def ft66(threadname, ports, portd):
+	print threadname , "is running"
+	global flag_mon66
+	global p2
+
+	while True:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.setblocking(0)
+		sock.settimeout(1.2)
+		tmp = sock.connect_ex(('127.0.0.1', ports))
+		if tmp != 0 and flag_mon66 != -1:
+			print 'Controller 1 detected down %s' %errno.errorcode[tmp]
+
+			flag_mon66 = -1
+
+			print 'SET ROLE C2 AS MASTER at %.10f' %time.time()
+			p2.stdin.write("import pox.openflow.nicira as nx\n")
+			p2.stdin.write("for connection in core.openflow.connections:\n")
+			p2.stdin.write("\tconnection.send(nx.nx_role_request(master='true'))\n")
+			p2.stdin.write('\n')
+
+			flag_mon67 = 0
+
+			while True:
+				p = subprocess.Popen(["ovs-vsctl", "-f", "csv", "list", "controller"], stdout=subprocess.PIPE)
+				output, err = p.communicate()
+				f = StringIO.StringIO(output)
+
+				reader = csv.reader(f, delimiter=',')
+				rownum = 0
+				con67 = []
+
+				for row in reader:
+					uuid = row[0]
+					target = row[15]
+					role = row[13]
+					i = target.find(str(portd))
+					if i != -1:
+						if (role == 'master'):
+							con67.append(uuid)
+
+				if len(con67) == HOSTS:
+					uptime = time.time()
+					print 'new master ready at %.10f' %uptime
+					flag_mon67 = 1
+					f.close()
+					break
+
+		sock.close()
+		
+		if flag_exit:
+			print 'exiting ',threadname
+			sys.exit()
+
+		time.sleep(1)
+
+def ft67(threadname,ports, portd):
+	print threadname , "is running"
+	global flag_mon67
+	
+	while True:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.setblocking(0)
+		sock.settimeout(1.2)
+		tmp = sock.connect_ex(('127.0.0.1', ports))
+
+		if tmp != 0:
+			print 'Controller 2 detected down %s' %errno.errorcode[tmp]
+
+			flag_mon67 = -1
+		
+		sock.close()
+		if flag_exit:
+			print 'exiting ',threadname
+			sys.exit()
+		time.sleep(1)
+
+    
+
+def myNet():
+	global p1
+	global p2
+	global p3
+	global p4
+
+	global flag_exit
+
+	cPort1=6666
+	cPort2=6667
+	hosts=[]
+
+	kill = 0
+
+	net = Mininet( topo=None, build=False, autoSetMacs=True)
+
+	con1 = net.addController( 'c0', controller=RemoteController, ip='127.0.0.1', port=cPort1)
+	con2 = net.addController( 'c1', controller=RemoteController, ip='127.0.0.1', port=cPort2)
+
+	for x in range(0, HOSTS):
+		hostname = "h%d" %(x)
+		switchname = "s%d" %(x)
+		host = net.addHost(hostname)
+		switch = net.addSwitch(switchname)
+
+		if (x!=0):
+			net.addLink(switch, lastswitch)
+			
+		lastswitch = switch
+		net.addLink(host,switch)
+
+		net.build()
+		switch.start([con1,con2])
+		hosts.append(host)
+
+	net.start()
+
+	flag_mon66 = flag_mon67 = 0
+	tmon66 = Thread( target=ft66, args=("ThreadMon66", cPort1, cPort2) )
+	tmon67 = Thread( target=ft67, args=("ThreadMon67", cPort2, cPort1) )
+
+	print "start monitoring thread"
+	tmon66.start()
+	tmon67.start()
+
+
+	# we shall wait for original master to become ready
+	while True:
+		p = subprocess.Popen(["ovs-vsctl", "-f", "csv", "list", "controller"], stdout=subprocess.PIPE)
+		output, err = p.communicate()
+		f = StringIO.StringIO(output)
+
+		reader = csv.reader(f, delimiter=',')
+		rownum = 0
+
+		con66 = [] # not using this for now
+		con67 = []
+
+		for row in reader:
+			uuid = row[0]
+			target = row[15]
+			role = row[13]
+			i = target.find(str(cPort1))
+			if i != -1:
+				if (role == 'master'):
+					con66.append(uuid)
+		f.close()
+		if len(con66) == HOSTS:
+			starttime = time.time()
+			print 'original master ready at %.10f' %starttime
+
+			flag_mon66 = 1
+			break
+
+	tping = time.time()
+	# print 'h0 ping : %.10f' % tping
+
+	# CLI(net)
+
+	for i in range(0, HOSTS/2):
+		tmping = time.time()
+		print i
+		dr = 2*i
+		print dr
+		to = dr+1
+		print to
+		print 'h%d ping h%d: %.10f' % (dr, to, tmping)
+		cmd = 'hping3 -c 200 -i u40000 %s > logs-example/log.ping%d%d.txt 2>&1 &' %(hosts[to].IP(), dr,to)
+		hosts[dr].cmdPrint(cmd) 
+
+	while True:
+		tcur = time.time()
+		if tcur - tping > 3: # after 2s running
+
+			# print 'SET ROLE C1 SLAVE '
+			# p1.stdin.write("import pox.openflow.nicira as nx\n")
+			# p1.stdin.write("for connection in core.openflow.connections:\n")
+			# p1.stdin.write("\tconnection.send(nx.nx_role_request(slave='true'))\n")
+			# p1.stdin.write('\n')
+
+			print 'close port %i in %.10f' %(cPort1,tcur)
+			closePort(cPort1)
+			break
+
+	# CLI(net)
+
+	# print 'SET ROLE C2 AS MASTER at %.10f' %time.time()
+	# p2.stdin.write("import pox.openflow.nicira as nx\n")
+	# p2.stdin.write("for connection in core.openflow.connections:\n")
+	# p2.stdin.write("\tconnection.send(nx.nx_role_request(master='true'))\n")
+	# p2.stdin.write('\n')
+
+	# while True:
+	# 	p = subprocess.Popen(["ovs-vsctl", "-f", "csv", "list", "controller"], stdout=subprocess.PIPE)
+	# 	output, err = p.communicate()
+	# 	f = StringIO.StringIO(output)
+
+	# 	reader = csv.reader(f, delimiter=',')
+	# 	rownum = 0
+
+	# 	con66 = [] # not using this for now
+	# 	con67 = []
+
+	# 	# print output
+	# 	# print '========================================================'
+
+	# 	for row in reader:
+	# 		uuid = row[0]
+	# 		target = row[15]
+	# 		role = row[13]
+	# 		i = target.find(str(cPort2))
+	# 		if i != -1:
+	# 			if (role == 'master'):
+	# 				con67.append(uuid)
+
+	# 	if len(con67) == HOSTS:
+	# 		uptime = time.time()
+	# 		print 'new master ready at %.10f' %uptime
+	# 		f.close()
+	# 		break
+
+
+	print 'now wait for hping3 to finish..'
+	
+	for i in range(0, HOSTS/2):
+		tmping = time.time()
+		dr = 2*i
+		hosts[dr].cmdPrint('wait %hping3')
+		print 'hping3 finished at %.10f' %time.time()
+
+	print 'open the port..'
+	unClosePort(cPort1)
+
+	print 'stopping mininet'
+	net.stop()
+	
+	print 'stopping pox(s)..'
+	p1.terminate()
+	p2.terminate()
+
+	print 'stopping monitoring module(s)..'
+	flag_exit = True
+	tmon66.join()
+	tmon67.join()
+
+	# print 'timestamp difference %.10f' %(uptime-tcur)
+
+if __name__ == '__main__':
+	setLogLevel( 'info' )
+	
+	p1 = subprocess.Popen(['pox/pox.py', "master66"],stdin=subprocess.PIPE, stdout=p1_log,stderr=p1_log,preexec_fn=os.setpgrp)
+	print 'c1 runs, master'
+	p2 = subprocess.Popen(['pox/pox.py',"slave67"],stdin=subprocess.PIPE, stdout=p2_log,stderr=p2_log,preexec_fn=os.setpgrp)
+	print 'c2 runs, slave'
+
+	print 'wait for 3 seconds...'
+	time.sleep(3)
+	
+	myNet()
+
+	print 'close pox logs..'
+	p1_log.close()
+	p2_log.close()
+
+	print 'bye'
+	# t.process.terminate()
